16ed48ad4df34f153adb4a191e331bf8daa695f4
SubrahmanyeswariKommireddy <59472440+SubrahmanyeswariKommireddy@users.noreply.github.com>
2020-01-30 13:06:49 +0530

    Update HotelsList.java

--- HotelsList.java
+++ HotelsList.java
@@ -59,3 +59,54 @@ public class HotelsList {
 		}
 	}
 }
+/*
+
+enter money how much money you want to spend
+100
+how many days
+3
+enter rating
+5
+Hotel Booked for :3 days in 5  star hotel The Park for the cost of 216.0
+.
+.
+.
+enter money how much money you want to spend
+30
+Sorry..! You don't have sufficient amount to book any hotel
+.
+.
+.
+enter money how much money you want to spend
+90
+how many days
+50
+ enter days , should not exceed a month
+30
+enter rating
+3
+Hotel Booked for :30 days in 3  star hotel Radisson for the cost of 1479.0
+.
+.
+.
+enter money how much money you want to spend
+60
+how many days
+4
+enter rating
+2
+please enter among 1 , 3 , 5 as star rating
+1
+Hotel Booked for :4 days in 1  star hotel Accord for the cost of 166.2
+.
+.
+.
+enter money how much money you want to spend
+400
+how many days
+3
+enter rating
+3
+Hotel Booked for :3 days in 3  star hotel RainTree for the cost of 147.9
+
+*/
